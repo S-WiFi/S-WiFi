1d7b790c3b7a069ce4e0fd57b0729ac8313f1f2a
Ping-Chun Hsieh <lleyfede@tamu.edu>
2016-02-23 23:48:28 -0600

    Update p4.m

--- report/p4.m
+++ report/p4.m
@@ -1,15 +1,16 @@
 % Problem 4: Plot CDF of delay against multiple retry counts.
-clean all; close all;
+clear; close all;
 
-K = 6
-f = cell(1, K);
+K = 1;
 figure; hold on;
 for k = 0:1:(K-1)
     fname = sprintf('swifi_delay_uplink_%d.dat', k);
-    if ~exist(fname)
+    if ~exist(fname, 'file')
         system(sprintf('ns swifi.tcl delay uplink %d', k));
-    y = load(fname);
-    [f{k}, x{k}] = ecdf(y);
-    plot(x{k}, f{k});
+    else
+        y = load(fname);
+        [f(k+1,:), x(k+1,:)] = ecdf(y);
+        %plot(x(k+1,:), f(k+1,:));
+    end
 end
-print('swifi_delay_cdf.eps');
+createfigure_P4(x,f);
