32e1cdc580d1192e1818594eb61509bfb894106b
pinghsieh <lleyfede@tamu.edu>
2016-04-26 20:58:05 -0500

    Add a script file for throughput of each client

---
+++ gen_throughput_d_each.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Run swifi.tcl against different distances (d), and get the list of
+# (timely-)throughput values.
+
+# Usage: ./gen_throughput_d.sh policy symmetry
+# where policy can be one of baseline (0), smart (7), or any number in between,
+# and symmetry can be one of sym or asym.
+
+policy=${1:-smart}
+symmetry=${2:-sym}
+
+for d in 1 `seq 200 200 2000`; do
+	ns swifi.tcl pcf $policy $d $symmetry
+	cat swifi_pcf_${symmetry}_${policy}_long.dat >> throughput_d_${symmetry}_${policy}_long.dat
+done
